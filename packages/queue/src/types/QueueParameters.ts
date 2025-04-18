88de3099f6bc21cdc09b491b0b9400cb3521f458
Olivier Lépine <olivier@lepine.fr>
2025-04-18 14:44:48 +0200

    Updated config parameters

--- packages/queue/src/types/QueueParameters.ts
+++ packages/queue/src/types/QueueParameters.ts
@@ -1,12 +1,12 @@
 export interface ConfigParameters {
+   // rabbitmq
    host?: string
    port?: number
    user?: string
    password?: string
-   credentials?: {
-      accesskey?: string
-      secret?: string
-   }
+   // aws sqs
+   accesskey?: string
+   secret?: string
    region?: string
    accountid?: string
 }
