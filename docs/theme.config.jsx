f265f3898b053c8b2f4bf9cf91f0d9e21707f198
Olivier <olivier@lepine.fr>
2026-05-08 22:02:07 +0200

    feat: initialize documentation site using Nextra and Next.js

---
+++ docs/theme.config.jsx
@@ -0,0 +1,13 @@
+export default {
+  logo: <span>Quatrain Core Documentation</span>,
+  project: {
+    link: 'https://github.com/Quatrain/Core'
+  },
+  search: {
+    placeholder: 'Search documentation...'
+  },
+  docsRepositoryBase: 'https://github.com/Quatrain/Core/tree/main/docs',
+  footer: {
+    text: 'Quatrain Core Documentation'
+  }
+}
