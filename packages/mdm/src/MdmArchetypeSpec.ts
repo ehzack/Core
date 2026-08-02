5e4c4f5614fcbf01267b1e82ce58725be3ee0dbd
Olivier <olivier@lepine.fr>
2026-08-02 11:58:58 +0200

    feat(mdm): add jsonSchema definition support to MdmArchetypeSpec

--- packages/mdm/src/MdmArchetypeSpec.ts
+++ packages/mdm/src/MdmArchetypeSpec.ts
@@ -3,15 +3,19 @@ import { OntologyMappingInterface } from './domain/OntologyDomain'
 
 /**
  * Specification Schema Definition for an Archetype
- * Supports recognized international ontologies (Schema.org, GS1 GPC, W3C SOSA, W3C WoT, ISO).
+ * Supports recognized international ontologies (Schema.org, GS1 GPC, W3C SOSA, W3C WoT, ISO)
+ * and standard JSON Schema definitions for specification groups.
  */
 export interface MdmArchetypeSpec {
    archetypeId: string
    name: string
    nature: MdmNature | string
    collection: string
+   typeCollection?: string
    /** Recognized International Ontology Mapping */
    ontologyMapping?: OntologyMappingInterface
+   /** Standard JSON Schema definition for specification groups & properties */
+   jsonSchema?: Record<string, unknown>
    /** Array of required specification property names */
    requiredProperties?: string[]
    /** Array of optional specification property names */
